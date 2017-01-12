4423ad2566c34e1062978eeafacb847681fbb9fc
StarshotProject <bartoszsamolyk2@gmail.com>
2017-01-12 22:56:27 +0100

    Delete Kontrola_poprawności_danych.java

--- GRA STATKI/src/Kontrola_poprawności_danych.java
+++
@@ -1,9 +0,0 @@
-//Klasa, która bêdzie kopiowaæ elementy gry i sprawdzaæ ich poprawnoœæ
-//metoda bêdzie przyjmowa³a za argument obiekt typu Gracz.
-//1. Czy wszystkie statki gracza s¹ umieszczone na planszy, na pocz¹tku wszystkie moga mieæ po³o¿enie poza plansz¹, nale¿y sprawdziæ,
-//czy wszystkie s¹ na planszy, oraz czy na siebie nie zachodz¹. Klasa bêdzie mia³a metody statyczne? odpalane po ka¿dym ruchu mog¹cym
-//wywo³aæ niepoprawnoœc danych
-public class Kontrola_poprawnoœci_danych {
-
-	
-}
